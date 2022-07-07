400247c72d7789abfef37b551b39a6c543900b73
Klein <jsun1470@gmail.com>
2022-07-07 10:03:50 +0800

    优点达项目初始化完成

--- project-ydd/src/router/index.js
+++ project-ydd/src/router/index.js
@@ -1,22 +1,61 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    redirect: '/home',
+    component: () => import('@/views/mobile'),
+    meta: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'home',
+        component: () => import('@/views/mobile/home'),
+        meta: {
+          title: '首页'
+        },
+      },
+      {
+        path: 'classify',
+        component: () => import('@/views/mobile/classify'),
+        meta: {
+          title: '分类'
+        },
+      },
+      {
+        path: 'release',
+        component: () => import('@/views/mobile/release'),
+        meta: {
+          title: '发布'
+        },
+      },
+      {
+        path: 'my',
+        component: () => import('@/views/mobile/my'),
+        meta: {
+          title: '我的'
+        },
+      },
+      {
+        path: 'login',
+        component: () => import('@/views/mobile/login'),
+        meta: {
+          title: '登录'
+        },
+      },
+      {
+        path: 'reg',
+        component: () => import('@/views/mobile/reg'),
+        meta: {
+          title: '注册'
+        },
+      },
+    ]
   }
 ]
 

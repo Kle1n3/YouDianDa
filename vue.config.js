3f39868748621b62a353162701d08032f2348f77
Klein <jsun1470@gmail.com>
2022-07-22 10:54:26 +0800

    submit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+const isPord = process.env.NODE_ENV === 'production'
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  publicPath: isPord ? './' : '/',  //处理 相对路径问题
+  productionSourceMap: false  // 优化不要map文件
 })
